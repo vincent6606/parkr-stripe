cfa89c718b849e5da5d895f7c2cefabff7e71d13
Zhi Ting Chen <zc346@cornell.edu>
2017-07-01 15:25:04 -0400

    add description

--- parameters formats.py
+++ parameters formats.py
@@ -3,18 +3,24 @@ def create_custom_account(first_name, last_name, dob, mob, yob,
                           bank_country='CA', bank_holder=None, bank_object="bank_account", bank_currency='cad', bank_type='individual',
                           token=None, currency="cad", country="CA", ip_address='8.8.8.8'):
 
-
+first_name/last name:
+	string
+dob:
+	string "24"
+mob:
+	string "06"
+yob:
+	string "1995"
 
 bank_account_num
+	dependent on Country
 
 bank_routing
 	For Canadian accounts:
-		routing number is an 8 digit routing number consisting of 5digit branch number and a 3 digit transit number
+		routing number is an 8 digit routing number consisting of 5 digit branch number and a 3 digit transit number
 		can be entered as "XXXXX-YYY" the dash is recognized by Stripe or "XXXXXYYY"
-
-
-
-
+	For American accounts:
+		routing number is a 9 digit number
 
 bank_country/country
 	a 2-letter capitalized countriy code(ISO 3166-1 alpha-2): "CA","US","UK"
@@ -24,6 +30,9 @@ bank_currency/currency
 	a 3-letter lower-case currency code "cad","usd"
 	http://www.xe.com/iso4217.php#A
 
+bank_holder:
+	name of the account holde it is first_name+' '+last name by default
+	
 bank_object="bank_account"
 	always "bank_account" if using a bank account, else, use a token
 
